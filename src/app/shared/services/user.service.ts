dc02390917948388b839370dd56e3f81e29793a1
Jessica Prieto <jessicaprieto31@gmail.com>
2018-11-12 00:03:59 -0500

    update github access token

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,39 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { UserModel } from '../models/user.model';
 
+const ACCESS_TOKEN = "e316b594b3e0d8bc033d58b64457f7d548b7b041";
+
 @Injectable()
 export class UserService {
     constructor(private http: HttpClient) { }
 
     githubUrl : string;
-    accessToken : string;
     allUsers : UserModel[]
 
 
     getUserSearchResult(searchedName : string) {
         if(searchedName != "") {
             this.githubUrl = "https://api.github.com/search/users?q=" + searchedName;
-            this.accessToken = "9632a2d06ed3785075f71b2d425a8c7abad27e88"
 
             return this.http.get(this.githubUrl, {
-                params: new HttpParams().set('access_token', this.accessToken) });
+                params: new HttpParams().set('access_token', ACCESS_TOKEN) });
         }        
     }
 
     getUser(username : string) {
         this.githubUrl = "https://api.github.com/users/" + username;
-        this.accessToken = "9632a2d06ed3785075f71b2d425a8c7abad27e88"
 
         return this.http.get(this.githubUrl, {
-            params: new HttpParams().set('access_token', this.accessToken) });
+            params: new HttpParams().set('access_token', ACCESS_TOKEN) });
     }
 
     getCommits(username : string) {
         this.githubUrl = "https://api.github.com/users/" + username + "/events";
-        this.accessToken = "9632a2d06ed3785075f71b2d425a8c7abad27e88"
 
         return this.http.get(this.githubUrl, {
-            params: new HttpParams().set('access_token', this.accessToken) });
+            params: new HttpParams().set('access_token', ACCESS_TOKEN) });
 
     }
 }
