315c25676dffa06434dc2517b6a7aed37fe06334
Jessica Prieto <jessicaprieto31@gmail.com>
2018-11-12 01:07:18 -0500

    update access tokens

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { UserModel } from '../models/user.model';
 
-const ACCESS_TOKEN = process.env.API_TOKEN;
+const client_id = "27912e19e6a989ab8f50";
+const client_secret = "0fa4826043d61831ea0e66a857ed6074de72ffcd";
 
 @Injectable()
 export class UserService {
@@ -14,25 +15,24 @@ export class UserService {
 
     getUserSearchResult(searchedName : string) {
         if(searchedName != "") {
-            this.githubUrl = "https://api.github.com/search/users?q=" + searchedName;
+            this.githubUrl = "https://api.github.com/search/users?q=" + searchedName 
+                + "&client_id=" + client_id 
+                + "&client_secret" + client_secret;
 
-            return this.http.get(this.githubUrl, {
-                params: new HttpParams().set('access_token', ACCESS_TOKEN) });
+            return this.http.get(this.githubUrl);
         }        
     }
 
     getUser(username : string) {
         this.githubUrl = "https://api.github.com/users/" + username;
 
-        return this.http.get(this.githubUrl, {
-            params: new HttpParams().set('access_token', ACCESS_TOKEN) });
+        return this.http.get(this.githubUrl);
     }
 
     getCommits(username : string) {
         this.githubUrl = "https://api.github.com/users/" + username + "/events";
 
-        return this.http.get(this.githubUrl, {
-            params: new HttpParams().set('access_token', ACCESS_TOKEN) });
+        return this.http.get(this.githubUrl);
 
     }
 }
